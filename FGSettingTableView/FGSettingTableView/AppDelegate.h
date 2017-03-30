228cb650aaf600235ec274c50bdc9e85f792da5a
GeeFarm <guopenghao@haogw.com>
2017-03-30 16:04:32 +0800

    first commit

---
+++ FGSettingTableView/FGSettingTableView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FGSettingTableView
+//
+//  Created by GPH on 17/3/30.
+//  Copyright © 2017年 GPH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
